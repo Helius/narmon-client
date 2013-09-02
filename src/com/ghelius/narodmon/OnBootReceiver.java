cf86f4a36cb87762983568428afc2658ecf63ed4
Eugene.Samoylov <Eugene.Samylov@eltex.loc>
2013-09-02 16:35:30 +0700

    fix watcher service interval setuping from onBoot devices, now interval get from preference

--- src/com/ghelius/narodmon/OnBootReceiver.java
+++ src/com/ghelius/narodmon/OnBootReceiver.java
@@ -7,6 +7,7 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.SystemClock;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 public class OnBootReceiver extends BroadcastReceiver {
@@ -19,9 +20,12 @@ public class OnBootReceiver extends BroadcastReceiver {
         Intent i = new Intent(context, OnAlarmReceiver.class);
         PendingIntent pi=PendingIntent.getBroadcast(context, 0, i, 0);
 
-        mgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
-                SystemClock.elapsedRealtime()+(1*60000), // 1 minute
-                (5*60000),
-                pi);
+	    mgr.setRepeating(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+			    SystemClock.elapsedRealtime() + (1 * 60000), // 1 minute
+			    (60000 * Integer.valueOf(PreferenceManager.
+					    getDefaultSharedPreferences(context).
+					    getString(context.getString(R.string.pref_key_interval), "5"))),
+			    pi);
+
     }
 }
