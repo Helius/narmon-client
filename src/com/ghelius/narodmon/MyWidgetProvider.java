4077e2557b14082b9b7993e8255034d0f464c662
Eugene.Samoylov <Eugene.Samylov@eltex.loc>
2014-02-05 18:24:06 +0700

    add dimens.xml for widget

--- src/com/ghelius/narodmon/MyWidgetProvider.java
+++ src/com/ghelius/narodmon/MyWidgetProvider.java
@@ -4,17 +4,18 @@ import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
 import android.util.Log;
 
 public class MyWidgetProvider extends AppWidgetProvider {
 
-	private static final String LOG = "narodmon-widgetProvider";
+	private static final String TAG = "narodmon-widgetProvider";
 
 	@Override
 	public void onUpdate(Context context, AppWidgetManager appWidgetManager,
 	                     int[] appWidgetIds) {
 		DatabaseHelper dbh = new DatabaseHelper(context);
-		Log.w(LOG, "onUpdate WidgetProvider's method called with " + appWidgetIds.length + "widgets");
+		Log.w(TAG, "onUpdate WidgetProvider's method called with " + appWidgetIds.length + "widgets");
 		// Build the intent to call the service
 		Intent intent = new Intent(context, UpdateWidgetService.class);
 		intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_IDS, new Integer[0]);
@@ -22,6 +23,13 @@ public class MyWidgetProvider extends AppWidgetProvider {
 		context.startService(intent);
 		dbh.close();
 	}
+
+    @Override
+    public void onAppWidgetOptionsChanged(Context context, AppWidgetManager appWidgetManager, int appWidgetId, Bundle newOptions) {
+        Log.d(TAG,"onAppWidgetOptionsChanged" + "");
+        //newOptions.
+    }
+
 	@Override
 	public void onDeleted(Context context, int[] appWidgetIds) {
 		super.onDeleted(context, appWidgetIds);
